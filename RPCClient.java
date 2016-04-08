b581a3ad0d896390f2af60e3efbdc606becbd147
Nianze <Nianze@users.noreply.github.com>
2016-04-08 01:08:32 -0400

    Update RPCClient.java
    operate as SSM:
    R = 2
    W = 3
    WQ = 2
    note: each write operation will randomly choose W instances to write and save the locMetaData to session value.

--- RPCClient.java
+++ RPCClient.java
@@ -4,18 +4,29 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-import java.util.ArrayList;
+import java.net.UnknownHostException;
+import java.util.*;
 
 public class RPCClient 
 {
 	
 	private ArrayList<InetAddress> requestAddress;
 	private final int portProj1bRPC = 5300;
+	private String[] locMetaData;  
+	private int R = 2;
+	private int W = 3;
+	private int WQ = 2;
 	
 	public RPCClient(ArrayList<InetAddress> list)
 	{
 		requestAddress = list;
 	}
+	
+	public RPCClient(String metaData) throws UnknownHostException{
+		locMetaData = metaData.split("_");
+	}
+	
+	public RPCClient(){}
 
 	public SessionValues sessionReadClient(String sessionID, Integer callID)
 	{
@@ -25,12 +36,20 @@ public class RPCClient
 			Integer operationCode = 0;
 			String request = callID + "_" + operationCode + "_" + sessionID + "_" + "dummyParam";
 			byte[] outBuf = new byte[256];
-			for(InetAddress addr: requestAddress)
-			{
+			for(int i = 0; i < R; i++){
+				// get R instances which has stored sessoin_value				
+				requestAddress = new ArrayList<InetAddress>();				
+				for(int j = 0; j < locMetaData.length; j++){
+					requestAddress.add(InetAddress.getByName(Globals.ipAddressMapping.get(locMetaData[j])));
+				}
+				// get random R instances' ip address 
+				Random randomizer = new Random();
+				InetAddress addr = requestAddress.get(randomizer.nextInt(requestAddress.size()));
 				DatagramPacket sendPkt = new DatagramPacket(outBuf, outBuf.length, addr, portProj1bRPC);
 				sendPkt.setData(request.getBytes());
 				rpcSocket.send(sendPkt);
 			}
+			
 			byte[] inBuf = new byte[256];
 			DatagramPacket recvPkt = new DatagramPacket(inBuf, inBuf.length);
 			Integer replyCallID = -1;
@@ -48,8 +67,7 @@ public class RPCClient
 				returnSessionID = parts[1];
 				version = Integer.parseInt(parts[2]);
 				message = parts[3];
-				expiredTS = parts[4];
-				
+				expiredTS = parts[4];				
 			} while(replyCallID != callID);
 			
 			rpcSocket.close();
@@ -74,16 +92,35 @@ public class RPCClient
 	{
 		try 
 		{
+			// randomly choose W instances to write new session
+			Random randomizer = new Random();
+			locMetaData = new String[W];
+			for(int j = 0; j < W; j++){				
+				locMetaData[j] = (String) Globals.ipAddressMapping.entrySet().toArray()[(randomizer.nextInt(Globals.ipAddressMapping.size()))];
+				requestAddress.add(InetAddress.getByName(Globals.ipAddressMapping.get(locMetaData[j])));
+			}
+			StringBuilder builder = new StringBuilder();
+			// create new location MetaData in session value
+			for(String s : locMetaData) {
+			    builder.append(s);
+			    builder.append("_");
+			}
+			
 			DatagramSocket rpcSocket = new DatagramSocket();
 			Integer operationCode = 1;
-			String request = callID + "_" + operationCode + "_" + sessionID + "_" + newMessage + "_" + "dummyParam";
+			String request = callID + "_" + operationCode + "_" + sessionID + "_" + newMessage + "_" + builder.toString();
 			byte[] outBuf = new byte[256];
-			for(InetAddress addr: requestAddress)
-			{
+
+			System.out.println("Before Sending!!");
+			for(int i = 0; i < WQ; i++){
+				// get random WQ instances' ip address 				
+				InetAddress addr = requestAddress.get(randomizer.nextInt(requestAddress.size()));
 				DatagramPacket sendPkt = new DatagramPacket(outBuf, outBuf.length, addr, portProj1bRPC);
 				sendPkt.setData(request.getBytes());
 				rpcSocket.send(sendPkt);
 			}
+			System.out.println("After Sending!!");
+			
 			byte[] inBuf = new byte[256];
 			DatagramPacket recvPkt = new DatagramPacket(inBuf, inBuf.length);
 			Integer replyCallID = -1;
@@ -91,8 +128,9 @@ public class RPCClient
 			String returnSessionID = "";
 			int version = -1;
 			String expiredTS = "";
-			do
-			{
+			
+			int wait4WQ = 0;
+			do{
 				rpcSocket.receive(recvPkt);
 				String response = new String(recvPkt.getData());
 				System.out.println("SPCClient Received Response: " + response);
@@ -102,11 +140,14 @@ public class RPCClient
 				version = Integer.parseInt(parts[2]);
 				message = parts[3];
 				expiredTS = parts[4];
-				
-			} while(replyCallID != callID);
+				if( replyCallID == callID ) wait4WQ++;
+			} while(wait4WQ < WQ);
 			
 			rpcSocket.close();
-			return new SessionValues(version, message, expiredTS);
+			
+			SessionValues sv = new SessionValues(version, message, expiredTS);
+			sv.locMetaData = builder.toString();
+			return sv;
 		}
 		catch (SocketException e) 
 		{
